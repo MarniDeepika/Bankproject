993483bc1b5d56ad3c02f8c2fab2146a13622f91
mdeepika <mdeepika@ITEM-S132987.emea.msad.sopra>
2022-09-22 15:53:19 +0530

    added deposit and withdraw methods

--- src/com/sopra/java/Bank.java
+++ src/com/sopra/java/Bank.java
@@ -1,5 +1,32 @@
 package com.sopra.java;
 
-public class Bank {
+public class Bank 
+{
+private int Deposit;
+private int withdraw;
 
+	
+		
+
+	public void withdraw(int l) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void printdata() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setdata() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Deposit(int k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
